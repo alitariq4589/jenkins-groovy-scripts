30ed06927a90c4969f8f7c3239d52c242b30522a
Ali Tariq <alitariq4589@gmail.com>
2024-06-07 17:08:37 +0500

    Added gitlab-branch-source plugin settings

---
+++ gitlab-branch-source/README.md
@@ -0,0 +1,6 @@
+# GitLab Branch source plugin
+
+This folder includes gitlab branch source plugin settings (e.g. if you want to add a server in global settings for multibranch pipeline).
+
+## How to use the script
+
---
+++ gitlab-branch-source/create_new_server.groovy
@@ -0,0 +1,35 @@
+import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServers
+import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServer
+import jenkins.model.Jenkins
+
+// Replace the following
+def gitlabServerName = 'my-gitlab-server'
+def gitlabServerUrl = 'https://gitlab.example.com'
+def credentialsId = 'your-credentials-id'           // Personal Access token from gitlab
+def secretTokenId = ''          // This is optional and you may not need it
+
+// Get the GitLabServers singleton
+def gitLabServers = GitLabServers.get()
+
+// Check if the GitLab server already exists
+def existingServer = gitLabServers.findServer(gitlabServerName)
+
+if (existingServer) {
+    println "GitLab server '${gitlabServerName}' already exists."
+} else {
+    // Create a new GitLab server configuration
+    def newServer = new GitLabServer(gitlabServerUrl, gitlabServerName, credentialsId)
+    
+    // Set additional properties
+    newServer.setManageWebHooks(true)
+    newServer.setManageSystemHooks(true)
+    newServer.setImmediateHookTrigger(true)
+    
+    // Set the webhook secret token ID
+    // newServer.setWebhookSecretCredentialsId(secretTokenId) // This may be optional
+
+    // Add the new server to the list of GitLab servers
+    gitLabServers.addServer(newServer)
+    Jenkins.instance.save()
+    println "GitLab server '${gitlabServerName}' added successfully."
+}
